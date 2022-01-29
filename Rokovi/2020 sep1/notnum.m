ed64235ec1d1cd93c15be7f5b8abfc770764d77a
Pavle Ciric <pavle.ciric@hotmail.com>
2022-01-29 12:20:39 +0100

    [add] 2020 sep1

---
+++ Rokovi/2020 sep1/notnum.m
@@ -0,0 +1,15 @@
+function S = notnum(a,k)
+  
+  if k < a
+    seg = k:a;
+    S = prod(seg(find(seg < (k+a)/2)));
+  elseif k >= a
+    S = ones(1,k);
+    S = S .* (a.^(1:k));
+    p = ones(1, k);
+    p(1) = a;
+    for i=2:k
+      S = [S; S(i-1,:).*p];
+      p(i) = a;
+    endfor
+  endif
---
+++ Rokovi/2020 sep1/num.m
@@ -0,0 +1,36 @@
+function I = num(n)
+  
+  podaci;
+  
+  C = cell(1,n);
+  C{1} = [1];
+  C{2} = [1 0];
+  
+  for i=3:n+1
+    C{i} = 2 * [C{i-1} 0] - [0 0 C{i-2}];
+  endfor
+  
+  celldisp(C);
+  
+  A = zeros(n);
+  b = zeros(n,1);
+  r = roots(C{n+1});
+  
+  g = @(x) 1./sqrt(1-x.^2);
+  
+  for i=1:n
+    A(i,:) = g(r);
+    b(i) = integral(g,-1,1);
+    g = @(x) g(x) .* x; 
+  endfor
+ 
+  f = @(x) f(x)./sqrt(1 - x.^2);
+  c = A\b;
+  I = sum(c .* f(r));
+  
+  X = linspace(-1,1);
+  hold on
+  for i=2:2:n+1
+    plot(X, polyval(C{i},X));
+  endfor
+  hold off
