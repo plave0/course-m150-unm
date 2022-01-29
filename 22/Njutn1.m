7c18c1f4ea95a86ca43d16ad5d2355e46a762206
Pavle Ciric <pavle.ciric@hotmail.com>
2022-01-29 11:13:24 +0100

    [add] 22

---
+++ 22/Njutn1.m
@@ -0,0 +1,21 @@
+function koef = Njutn1()
+  
+  tablica;
+  
+  n = length(X);
+  prazlike = zeros(n,n);
+  
+  prazlike(:,1) = F(:);
+  for i=2:n
+    prazlike(1:n-i+1,i) = diff(prazlike(1:n-i+2,i-1));
+  endfor
+  
+  p = [1 0];
+  Pn = F(1);
+  
+  for i=1:n-1
+    Pn = [0 Pn] + p * prazlike(1, i+1) / factorial(i);
+    p = conv(p, [1 -i]);
+  endfor  
+  
+  koef = Pn;
---
+++ 22/nula.m
@@ -0,0 +1,27 @@
+function [I,Y,x] = nula(x0, xF, tol, iterM)
+  
+  tablica;
+  h = X(2) - X(1);
+  
+  Pn = Njutn1();
+  f = @(x) polyval(Pn, (x - X(1))/h);
+ 
+  x = 0;
+  Y = [x0];
+  I = [1];
+  i = 1;
+  while 1
+    x1 = x0 - f(x0)/(f(xF) - f(x0)) * (xF - x0);
+    Y = [Y x1];
+    i = i + 1;
+    I = [I i];
+    if abs(x1 - x0) < tol
+      x = x1;
+      disp('Zadovoljena je tacnost');
+      break;
+    elseif i >= iterM
+      disp('Dostignut je maksimalni broj iteracija');
+      break;
+    endif
+    x0 = x1;
+  endwhile
